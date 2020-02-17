4220a8e2a4582edadc2ced286c2c97de2e964ff5
cristinaStratan1 <56672529+cristinaStratan1@users.noreply.github.com>
2020-02-17 16:37:27 +0100

    Update Paho.java

--- kafka_project/src/kafka_project/Paho.java
+++ kafka_project/src/kafka_project/Paho.java
@@ -77,9 +77,5 @@ public class Paho {
 
 	}
 
-	static String readFile(String path, Charset encoding) throws IOException {
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		return new String(encoded, encoding);
-	}
 }
 
